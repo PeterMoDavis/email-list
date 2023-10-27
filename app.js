3420b59722e6a6e86efce6debeaab8e74afbaf1e
Peter MoDavis <pmodavis.webdev@gmail.com>
2023-10-26 21:41:06 -0400

    basic functionality added.  Name and email is logged.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const submitButton = document.querySelector('#submit-button');
 
+function retrieveInput() {
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name.value}  Email: ${email.value}`);
+  });
+}
+
+retrieveInput();
