f8c080f125d21ecc21d83c613ffb0ff7635909ab
Peter MoDavis <pmodavis.webdev@gmail.com>
2023-12-14 21:15:00 -0500

    added functionality to see a confirmation of the email added with style

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ document
     const firstName = document.getElementById('first-name').value;
     const lastName = document.getElementById('last-name').value;
     const email = document.getElementById('email').value;
+    const congratulations = document.querySelector('.congratulations');
+    const overlay = document.querySelector('.dimmed-overlay');
+    const inputs = document.querySelectorAll('input');
 
     const response = await fetch('http://localhost:8000/saveUser', {
       method: 'POST',
@@ -16,7 +19,14 @@ document
     });
 
     const data = await response.json();
-    console.log(data);
+
+    congratulations.classList.add('show');
+    overlay.classList.add('show');
+
+    setTimeout(() => {
+      congratulations.classList.remove('show');
+      overlay.classList.remove('show');
+    }, 3000);
 
     document.querySelector('#userForm').reset();
   });
