5a85efed374648e01f142ae7bf40bf08c9161cc9
Peter MoDavis <pmodavis.webdev@gmail.com>
2023-12-14 21:52:44 -0500

    added conditional statements to block blank inputs

--- app.js
+++ app.js
@@ -10,23 +10,38 @@ document
     const overlay = document.querySelector('.dimmed-overlay');
     const inputs = document.querySelectorAll('input');
 
-    const response = await fetch('http://localhost:8000/saveUser', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ firstName, lastName, email }),
-    });
+    let allInputsFilled = true;
 
-    const data = await response.json();
+    for (const input of inputs) {
+      console.log(input.value);
+      if (input.value === '') {
+        allInputsFilled = false;
+        break; // Break the loop if any input is empty
+      }
+    }
 
-    congratulations.classList.add('show');
-    overlay.classList.add('show');
+    if (allInputsFilled) {
+      const response = await fetch('http://localhost:8000/saveUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ firstName, lastName, email }),
+      });
+      const data = await response.json();
+
+      congratulations.innerText = `Congratulations ${firstName}, you've entered your email!!!`;
+      congratulations.classList.add('show');
+      overlay.classList.add('show');
+    } else {
+      congratulations.innerText = 'You have not filled out all the inputs';
+      congratulations.classList.add('show');
+      overlay.classList.add('show');
+    }
 
     setTimeout(() => {
       congratulations.classList.remove('show');
       overlay.classList.remove('show');
+      document.querySelector('#userForm').reset();
     }, 3000);
-
-    document.querySelector('#userForm').reset();
   });
